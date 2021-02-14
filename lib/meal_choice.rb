d0a3fa3f57cc50975a4683c01c0f89252dc53950
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-14 20:57:27 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 # Your code here
-def meal_choice(veg1,veg2,veg3)
+def meal_choice(veg1,veg2,protein="tofu")
   puts "What a nutritious meal!A plate of #{veg1} with #{veg2} and #{veg3}."
 
 end
