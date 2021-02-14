93ef49ab98181b46ad59a7eface172e5e36ac87d
Daulen Zhangabylov <daulenz@gmail.com>
2021-02-14 20:55:37 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,6 @@ require 'pry'
 # Your code here
 def meal_choice(veg1,veg2,veg3)
   puts "What a nutritious meal!A plate of #{veg1} with #{veg2} and #{veg3}."
-binding.pry
+
 end
 meal_choice('protein','carrots', 'string beans')
